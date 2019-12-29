2590075b4f60c6867aca23bf0aff3c9230263386
Antonio Napolitano <nap@antonionapolitano.eu>
2019-12-29 23:55:42 +0100

    Bug fix.

--- udp_proxy_launcher.py
+++ udp_proxy_launcher.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-import udp_proxy
+from udp_proxy import launch
+from sys import argv
 
-while True:
-    try:
-        pass
-    except KeyboardInterrupt:
-        break
+def main():
+    launch(argv[1])
 
-exit(0)
+if __name__ == "__main__":
+    main()
