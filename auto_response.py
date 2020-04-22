770624f733b38f22f4a57dc14f259153d2f57aa5
Antonio Napolitano <nap@antonionapolitano.eu>
2020-04-22 16:10:24 +0200

    Cleanup old code.

--- auto_response.py
+++ auto_response.py
@@ -7,26 +7,6 @@ from queue import LifoQueue
 server_port = 9600
 server_socket = socket(AF_INET, SOCK_DGRAM)
 server_socket.bind(('0.0.0.0', server_port))
-client_socket = socket(AF_INET, SOCK_DGRAM)
-client_address_queue = LifoQueue()
-server_address = ""
-
-def handle_client():
-    while True:
-        client_message, client_address = server_socket.recvfrom(2048)
-        client_socket.sendto(client_message, (server_address, 9600))
-        client_address_queue.put(client_address)
-
-def send_client():
-    while True:
-        pass
-
-def handle_server():
-    while True:
-        client_address = client_address_queue.get()
-        server_message, server_address = client_socket.recvfrom(2048)
-        server_socket.sendto(server_message, client_address)
-        client_address_queue.task_done()
 
 def launch(server_addr):
     server_address = server_addr
@@ -34,9 +14,3 @@ def launch(server_addr):
     client_message, client_address = server_socket.recvfrom(2048)
     print("Sending magic packet...")
     server_socket.sendto(b'\xc8\x91\x1f', client_address)
-
-    #client_handler = Thread(target=handle_client)
-    #server_handler = Thread(target=handle_server)
-    #print("Listening...")
-    #client_handler.start()
-    #server_handler.start()
